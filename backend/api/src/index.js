95d71792eba962d36171d6e9e95d7f68c2d5fcd9
Mirza <kadric681@gmail.com>
2024-12-19 16:29:11 +0100

    Created Starting code for backend startup

---
+++ backend/api/src/index.js
@@ -0,0 +1,23 @@
+// src/index.js
+
+const express = require('express');
+
+const app = express();
+
+
+// Middleware
+app.use(express.json());
+
+// Routes
+const authenticationRoutes = require('./routes/authenticationRoutes');
+
+
+app.use('/api/authentication', authenticationRoutes);
+
+
+const PORT = process.env.PORT || 5001;
+
+
+app.listen(PORT, () => {
+    console.log(`Backend server running on port ${PORT}`);
+});
--- backend/api/src/routes/authenticationRoutes.js
+++ backend/api/src/routes/authenticationRoutes.js
@@ -4,4 +4,13 @@ const express = require('express');
 const router = express.Router();
 
 
+//path to Authentication Controller
 const authneticationController = require('../controllers/authenticationController');
+
+
+//Api Routes
+router.post('api/login', authneticationController.login);
+
+
+
+module.exports = router;
